9b2d78cb7160ce8b2e775be19d4e064a37987a8f
theydy <ydythebs@qq.com>
2018-12-27 23:54:23 +0800

    18.12.27

---
+++ JavaScript/78. 子集.js
@@ -0,0 +1,39 @@
+let checkNotInRes = function (a, res) {
+  if (res.findIndex(
+    (item) => JSON.stringify(item) === JSON.stringify(a)) === -1
+  ) {
+    return true;
+  }
+  return false;
+}
+
+let dfs = function (nums, res, cur) {
+  let a = [...cur];
+  a.sort();
+  if (checkNotInRes(a, res)) {
+    res.push(a);
+  }
+  for(let i = 0; i < nums.length; i++) {
+    if (cur.indexOf(nums[i]) === -1) {
+      cur.push(nums[i]);
+      let a = [...cur];
+      a.sort();
+      if (checkNotInRes(a, res)) {
+        dfs(nums, res, cur);
+      }
+      cur.pop();
+    }
+  }
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  // dfs 巨慢，以后改成位运算吧
+  let res = [];
+  let cur = [];
+  dfs(nums, res, cur);
+  return res;
+};
