4ec5687bd4de54ee47a89a05f3796022b2066250
theydy <ydythebs@qq.com>
2019-01-07 23:38:01 +0800

    Update 236. 二叉树的最近公共祖先.js

--- JavaScript/236. 二叉树的最近公共祖先.js
+++ JavaScript/236. 二叉树的最近公共祖先.js
@@ -12,33 +12,16 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-  let pFlag, qFlag;
-  const pParentNode = [];
-  const qParentNode = [];
-  const findTreeNode = function (root) {
-    if (!root || (pFlag && qFlag)) {
-      return;
-    }
-    if (!pFlag) {
-      pParentNode.push(root);
-      root.val === p.val && (pFlag = true);
-    }
-    if (!qFlag) {
-      qParentNode.push(root);
-      root.val === q.val && (qFlag = true);
-    }
-    findTreeNode(root.left);
-    findTreeNode(root.right);
-    !pFlag && pParentNode.pop();
-    !qFlag && qParentNode.pop();
-  };
-  findTreeNode(root);
-  let len = Math.min(pParentNode.length, qParentNode.length);
-  while (len > 0) {
-    if (pParentNode[len - 1].val === qParentNode[len - 1].val) {
-      return pParentNode[len - 1];
-    }
-    len--;
+  if (root === null || root.val === p.val || root.val === q.val) {
+    return root;
   }
-  return root;
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+  if (!left && !right) {
+    return null;
+  } else if (left && right) {
+    return root;
+  } else {
+    return left ? left : right;
+  }
 };
