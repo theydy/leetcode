22513fdabd5cf79e73b9f0c4e0ba3b57ab3e41ad
theydy <ydythebs@qq.com>
2019-02-18 19:06:48 +0800

    19.2.18

---
+++ JavaScript/16. 最接近的三数之和.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+  nums.sort((a, b) => (a - b));
+  let l, r, sum;
+  let res = nums[0] + nums[1] + nums[2];
+  for (let i = 0; i < nums.length - 2; i++) {
+  	l = i + 1;
+  	r = nums.length - 1;
+  	while (l < r) {
+  	  sum = nums[l] + nums[i] + nums[r];
+  	  res = Math.abs(target - sum) < Math.abs(target - res) ? sum : res;
+      if (sum === target) {
+        return sum;
+      } else if (sum > target) {
+        r--;
+      } else {
+        l++;
+      }
+  	}
+  }
+  return res;
+};
