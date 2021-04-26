7b8abf487075dbf35d8a52a399aa2329bfe1b7c6
theydy <ydythebs@qq.com>
2021-04-26 22:43:07 +0800

    Create 103. 二叉树的锯齿形层序遍历.ts

---
+++ rebirth/栈-队列/103. 二叉树的锯齿形层序遍历.ts
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+type StackNode = { dept: number, node: TreeNode | null };
+
+function createStackNode (node: TreeNode | null, dept: number): StackNode {
+  return {
+    node,
+    dept,
+  }
+}
+
+function zigzagLevelOrder(root: TreeNode | null): number[][] {
+  const stack: StackNode[] = [createStackNode(root, 0)];
+  const res: number[][] = [];
+
+  while(stack.length) {
+    const { node, dept } = stack.shift();
+
+    if (!node) continue;
+
+    node.left && stack.push(createStackNode(node.left, dept + 1));
+    node.right && stack.push(createStackNode(node.right, dept + 1));
+
+    res[dept] || (res[dept] = []);
+
+    if (dept % 2) {
+      res[dept].unshift(node.val);
+    } else {
+      res[dept].push(node.val);
+    }
+  }
+
+  return res;
+};
