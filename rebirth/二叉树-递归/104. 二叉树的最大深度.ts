6181b2b833fa375a8c60f3c89049f1af82a4a740
theydy <ydythebs@qq.com>
2021-04-27 23:15:35 +0800

    Create 104. 二叉树的最大深度.ts

---
+++ rebirth/二叉树-递归/104. 二叉树的最大深度.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+  let deep = 0;
+
+  const traverse = (root: TreeNode | null, dept: number = 1) => {
+    if (!root) return;
+
+    deep = Math.max(dept, deep);
+
+    traverse(root.left, dept + 1);
+    traverse(root.right, dept + 1);
+  }
+
+  traverse(root);
+
+  return deep;
+};
