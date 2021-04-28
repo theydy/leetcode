a064ba3663c76b299f524a6ea1ff1be4421a9222
theydy <ydythebs@qq.com>
2021-04-28 19:43:55 +0800

    Create 257. 二叉树的所有路径.ts

---
+++ rebirth/二叉树-递归/257. 二叉树的所有路径.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function binaryTreePaths(root: TreeNode | null): string[] {
+  if (!root) return [''];
+
+  let res: string[] = [];
+
+  const traverse = (root: TreeNode | null, paths: string = '') => {
+    if (!root) return;
+
+    paths = paths ? (paths + '->' + root.val) : String(root.val);
+
+    if (!root.left && !root.right) {
+      res.push(paths);
+    } 
+
+    traverse(root.left, paths);
+    traverse(root.right, paths);
+  }
+
+  traverse(root);
+
+  return res;
+};
