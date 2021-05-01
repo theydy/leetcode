96a86f8bcf8dede88f03219ed1b7c387a6c90107
theydy <ydythebs@qq.com>
2021-05-01 23:55:37 +0800

    Update 70. 爬楼梯.ts

--- rebirth/动态规划/70. 爬楼梯.ts
+++ rebirth/动态规划/70. 爬楼梯.ts
@@ -1,8 +1,7 @@
 function climbStairs(n: number): number {
   let j = 1, k = 2, temp;
 
-  if (n == 1) return j;
-  if (n === 2) return k;
+  if (n <= 2) return n;
 
   for(let i = 3; i <= n; i++) {
     temp = j + k;
